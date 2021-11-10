7892965d2193b061fa768b64413b178a6fd2dd77
vedku <57610056+vedku@users.noreply.github.com>
2021-11-10 21:31:20 +0800

    Create passwordmaker.py

---
+++ Useful/passwordmaker.py
@@ -0,0 +1,6 @@
+usable_characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',"$", "#", "@", "^", "&",'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', ]
+password = ""
+import random
+for i in range(8): 
+    password += random.choice(usable_characters)
+print(password)
