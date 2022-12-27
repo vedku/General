e6cf5054da0c053328f5dd5610d417ebb13bcade
vedku <57610056+vedku@users.noreply.github.com>
2022-12-27 17:57:19 +0800

    Update main.py

--- Useful/Weight Plotter/main.py
+++ Useful/Weight Plotter/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
-start_date_str = input("Enter the start date (YYYY-MM-DD): ")
+start_date_str = input("Enter the start date (YYYY-MM-DD):")
 start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
 weights = []
 weight = float(input("Enter weight for start date:"))
@@ -19,8 +19,14 @@ while True:
 num_days = len(weights)
 dates = [start_date + timedelta(days=i) for i in range(num_days)]
 date_labels = [date.strftime("%Y-%m-%d") for date in dates]
+max_weight = max(weights)
+
 y_min = float(input("Enter minimum value for y-axis:"))
 y_max = float(input("Enter maximum value for y-axis:"))
+if y_min < 0:
+    y_min = 0
+if y_max < max_weight:
+    y_max = max_weight
 
 plt.plot(dates, weights)
 plt.xlabel("Date")
