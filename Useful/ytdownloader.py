70c7baf83aa0a39bde3f34ee32ad3fd5ea6f1a08
Ved Kuwadekar <57610056+vedku@users.noreply.github.com>
2024-04-04 19:11:08 +0800

    Update ytdownloader.py to avoid ratelimit

--- Useful/ytdownloader.py
+++ Useful/ytdownloader.py
@@ -1,13 +1,20 @@
 from pytube import Playlist
+import time
 
-def download_playlist(playlist_url, path='Downloads/'): #replace with your path
+def download_playlist(playlist_url, path='C:/Users/YourUsername/Desktop/'): #modify the path
     playlist = Playlist(playlist_url)
-
+    print(f'Found {len(playlist.videos)} videos in the playlist.')
+    
     for video in playlist.videos:
-        print(f'Downloading: {video.title}')
-        video.streams.get_highest_resolution().download(output_path=path)
-        print(f'Finished downloading: {video.title}')
+        try:
+            print(f'Downloading: {video.title}')
+            video.streams.get_highest_resolution().download(output_path=path)
+            print(f'Finished downloading: {video.title}')
+            time.sleep(5)  #anti rate limit
+        except Exception as e:
+            print(f'Failed to download {video.title}: {e}')
 
 if __name__ == '__main__':
-    playlist_url = 'pass'  #your url here
-    download_playlist(playlist_url)
+    playlist_url = 'YOUR_PLAYLIST_URL_HERE'  #replace with your url
+    download_playlist(playlist_url, path='C:/Users/YourUsername/Desktop/')  #modify the path
+
